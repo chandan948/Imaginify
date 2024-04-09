76befd24cccf32fabd4191ed669867020df1d378
Chandan Kumar Tiwari <136148366+chandan948@users.noreply.github.com>
2024-04-09 15:49:36 +0530

    uploaded

---
+++ marketplace-v3/const/contractAddresses.ts
@@ -0,0 +1,17 @@
+/** Replace the values below with the addresses of your smart contracts. */
+
+// 1. Set up the network your smart contracts are deployed to.
+// First, import the chain from the package, then set the NETWORK variable to the chain.
+import { Mumbai } from "@thirdweb-dev/chains";
+export const NETWORK = Mumbai;
+
+// 2. The address of the marketplace V3 smart contract.
+// Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
+export const MARKETPLACE_ADDRESS = "0x6f48B46e81692fEcc7e54245d85e24f1fD61ecAb";
+
+// 3. The address of your NFT collection smart contract.
+export const NFT_COLLECTION_ADDRESS ="0x1aB7ED197A9EA4B6b3aFe433E86318C40865Cad0";
+
+// (Optional) Set up the URL of where users can view transactions on
+// For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
+export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
---
+++ marketplace-v3/pages/index.tsx
@@ -0,0 +1,70 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+/**
+ * Landing page with a simple gradient background and a hero asset.
+ * Free to customize as you see fit.
+ */
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.hero}>
+          <div className={styles.heroBackground}>
+            <div className={styles.heroBackgroundInner}>
+              <Image
+                src="/hero-gradient.png"
+                width={900}
+                height={1100}
+                alt="Background gradient"
+                quality={100}
+                className={styles.gradient}
+              />
+            </div>
+          </div>
+          <div className={styles.heroAssetFrame}>
+            <Image
+              src="/hero-asset.png"
+              width={860}
+              height={540}
+              alt="Hero asset, NFT marketplace"
+              quality={100}
+              className={styles.heroAsset}
+            />
+          </div>
+          <div className={styles.heroBodyContainer}>
+            <div className={styles.heroBody}>
+              <h1 className={styles.heroTitle}>
+                <span className={styles.heroTitleGradient}>
+                  SU NFT Marketplace
+                </span>
+                <br />
+              </h1>
+              <p className={styles.heroSubtitle}>
+                Created by <b>Harsh, Chandan, and Jaideep</b> guided by <b><i>Mrs. Ekta Singh</i></b>.
+              </p>
+
+              <div className={styles.heroCtaContainer}>
+                <Link className={styles.heroCta} href="/buy">
+                  Get Started
+                </Link>
+                <Link
+                  className={styles.secondaryCta}
+                  href=" "
+                  target="_blank"
+                >
+                  Mint from here!
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+
